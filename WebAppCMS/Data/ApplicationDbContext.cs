41fad8dc926e47704a24aa9da6671d1d4bd96d8f
mgazdovic <mislav.gazdovic@gmail.com>
2022-06-16 19:46:26 +0200

    Updated ApplicationDbContext

--- WebAppCMS/Data/ApplicationDbContext.cs
+++ WebAppCMS/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WebAppCMS.Models;
 
 namespace WebAppCMS.Data
 {
@@ -12,5 +13,9 @@ namespace WebAppCMS.Data
             : base(options)
         {
         }
+        /* TODO: dodati seed u migraciju! (Entity.HasData ...) za jednog admin i jednog obicnog usera */
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Category> Category { get; set; }
     }
 }
