4f783c5e57d63c73c78e8b220d2efafbdac0de07
mgazdovic <mislav.gazdovic@gmail.com>
2022-06-16 19:51:00 +0200

    Added Category handling (Controller, Views)

---
+++ WebAppCMS/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAppCMS.Data;
+using WebAppCMS.Models;
+
+namespace WebAppCMS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Category
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Category.Include(c => c.ModifiedBy).ToListAsync());
+        }
+
+        // GET: Admin/Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Category/Create
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.CreatedAt = DateTime.Now;
+                category.ModifiedAt = DateTime.Now;
+                category.ModifiedBy = _context.Users.FirstOrDefault(u => u.Id == GetCurrentUserId());
+
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Category/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Admin/Category/Edit/5
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,CreatedAt")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                category.ModifiedAt = DateTime.Now;
+                category.ModifiedBy = _context.Users.FirstOrDefault(u => u.Id == GetCurrentUserId());
+
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Admin/Category/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category
+                .Include(c => c.ModifiedBy)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Admin/Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Category.FindAsync(id);
+            _context.Category.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Category.Any(e => e.Id == id);
+        }
+
+        private string GetCurrentUserId()
+        { 
+            return User.FindFirst(ClaimTypes.NameIdentifier)?.Value.ToString();
+        }
+    }
+}
