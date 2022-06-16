42b72bb340766aeed6282b93582c4c2da1190c1f
mgazdovic <mislav.gazdovic@gmail.com>
2022-06-16 19:45:13 +0200

    Added Models (Category, Product)

---
+++ WebAppCMS/Models/Category.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WebAppCMS.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        [Display(Name="Created")]
+        public DateTime CreatedAt { get; set; }
+
+        [Display(Name = "Modified")]
+        public DateTime ModifiedAt { get; set; }
+
+        [Display(Name = "Modified By")]
+        public IdentityUser ModifiedBy { get; set; }
+    }
+}
---
+++ WebAppCMS/Models/Product.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace WebAppCMS.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Category")]
+        public string CategoryName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 2)]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(9,2)")]
+        [Display(Name = "Unit Price")]
+        public decimal UnitPrice { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTime CreatedAt { get; set; }
+
+        [Display(Name = "Modified")]
+        public DateTime ModifiedAt { get; set; }
+
+        [Display(Name = "Modified By")]
+        public IdentityUser ModifiedBy { get; set; }
+    }
+}
